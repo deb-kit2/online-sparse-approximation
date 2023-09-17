2406903432bf34aa5ac72428cd1b325d1f2b6710
Deb <nick.kittu.deb@gmail.com>
2023-09-17 10:53:04 +0530

    bug fixes

--- relaxed.py
+++ relaxed.py
@@ -1,6 +1,6 @@
 import copy
 import numpy as np
-import tqdm
+from tqdm import tqdm
 
 
 def hard_threshold(vector, sparsity : int) :
@@ -36,7 +36,7 @@ def htp(phi, y, K, mu = 0.3, max_iter = 100) :
         L = np.argsort(np.abs(x + mu * phi.T @ (y - phi @ x)))[::-1][:K]
         
         x_new = np.zeros(N)
-        x_new[L] = np.linalg.lstsq(phi[:, L], y, rconf = None)[0]
+        x_new[L] = np.linalg.lstsq(phi[:, L], y, rcond = None)[0]
 
         if np.linalg.norm(x_new - x) < tol * np.linalg.norm(x) :
             break
@@ -140,7 +140,7 @@ def sp(phi, y, K, max_iter = 25) :
 
         tl_new = np.argsort(np.abs(x_p))[::-1][:K]
 
-        if tl == tl_new :
+        if (tl == tl_new).all() :
             break
         
         r = residual(y, phi[:, tl_new])
@@ -172,7 +172,7 @@ def solver(max_support_size, support_size, phi, T) :
         "subspace" : []
         }
     Y = np.zeros(M)
-    U = np.zeors(N)
+    U = np.zeros(N)
 
     for t in tqdm(range(1, T + 1)) :
         # u_t
@@ -198,6 +198,6 @@ def solver(max_support_size, support_size, phi, T) :
         errors["htp"].append(np.mean((expected - h) ** 2))
         errors["ihtp"].append(np.mean((expected - ih) ** 2))
         errors["cosamp"].append(np.mean((expected - c) ** 2))
-        errors["sp"].append(np.mean((expected - s) ** 2))
+        errors["subspace"].append(np.mean((expected - s) ** 2))
 
     return errors
